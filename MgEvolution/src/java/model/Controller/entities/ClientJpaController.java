e63751b1369242c2bb1d0d5fba079bb48e07df5a
Judith Romero <63924732+judithromero23@users.noreply.github.com>
2021-10-21 19:03:24 +0200

    Add files via upload

---
+++ MgEvolution/src/java/model/Controller/entities/ClientJpaController.java
@@ -0,0 +1,167 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller.entities;
+
+import Controllers.exceptions.NonexistentEntityException;
+import Controllers.exceptions.RollbackFailureException;
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Query;
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.EntityTransaction;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import model.entities.Client;
+
+/**
+ *
+ * @author judith
+ */
+public class ClientJpaController implements Serializable {
+
+    public ClientJpaController(EntityManagerFactory emf) {
+        this.emf = emf;
+    }
+    
+    private EntityManagerFactory emf = null;
+
+    public EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+
+    public void create(Client client) throws RollbackFailureException, Exception {
+        EntityManager em = null;
+        EntityTransaction etx = null;
+        try {
+            em = getEntityManager();
+            etx = em.getTransaction();
+            etx.begin();
+            em.persist(client);
+            etx.commit();
+        } catch (Exception ex) {
+            try {
+                ex.printStackTrace();
+                etx.rollback();
+            } catch (Exception re) {
+                throw new RollbackFailureException("An error occurred attempting to roll back the transaction.", re);
+            }
+            throw ex;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public void edit(Client client) throws NonexistentEntityException, RollbackFailureException, Exception {
+         EntityManager em = null;
+         EntityTransaction etx = null;
+        try {
+            em = getEntityManager();
+            etx = em.getTransaction();
+            etx.begin();
+            client = em.merge(client);
+            etx.commit();
+        } catch (Exception ex) {
+            try {
+                etx.rollback();
+            } catch (Exception re) {
+                throw new RollbackFailureException("An error occurred attempting to roll back the transaction.", re);
+            }
+            String msg = ex.getLocalizedMessage();
+            if (msg == null || msg.length() == 0) {
+                Long id = client.getId();
+                if (findClient(id) == null) {
+                    throw new NonexistentEntityException("The client with id " + id + " no longer exists.");
+                }
+            }
+            throw ex;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public void destroy(Long id) throws NonexistentEntityException, RollbackFailureException, Exception {
+        EntityManager em = null;
+        EntityTransaction etx = null;
+        try {
+            em = getEntityManager();
+            etx = em.getTransaction();
+            etx.begin();
+            Client client;
+            try {
+                client = em.getReference(Client.class, id);
+                client.getId();
+            } catch (EntityNotFoundException enfe) {
+                throw new NonexistentEntityException("The client with id " + id + " no longer exists.", enfe);
+            }
+            em.remove(client);
+            etx.commit();
+        } catch (Exception ex) {
+            try {
+                etx.rollback();
+            } catch (Exception re) {
+                throw new RollbackFailureException("An error occurred attempting to roll back the transaction.", re);
+            }
+            throw ex;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public List<Client> findClientEntities() {
+        return findClientEntities(true, -1, -1);
+    }
+
+    public List<Client> findClientEntities(int maxResults, int firstResult) {
+        return findClientEntities(false, maxResults, firstResult);
+    }
+
+    private List<Client> findClientEntities(boolean all, int maxResults, int firstResult) {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            cq.select(cq.from(Client.class));
+            Query q = em.createQuery(cq);
+            if (!all) {
+                q.setMaxResults(maxResults);
+                q.setFirstResult(firstResult);
+            }
+            return q.getResultList();
+        } finally {
+            em.close();
+        }
+    }
+
+    public Client findClient(Long id) {
+        EntityManager em = getEntityManager();
+        try {
+            return em.find(Client.class, id);
+        } finally {
+            em.close();
+        }
+    }
+
+    public int getClientCount() {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            Root<Client> rt = cq.from(Client.class);
+            cq.select(em.getCriteriaBuilder().count(rt));
+            Query q = em.createQuery(cq);
+            return ((Long) q.getSingleResult()).intValue();
+        } finally {
+            em.close();
+        }
+    }
+    
+}
