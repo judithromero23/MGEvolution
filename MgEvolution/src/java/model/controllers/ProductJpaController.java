7d93f53acaad3f109711d7dbe72b566c7680f774
Judith Romero <63924732+judithromero23@users.noreply.github.com>
2021-10-21 19:28:13 +0200

    JPAController, Exceptions
    Client, Product, Stylist

---
+++ MgEvolution/src/java/model/controllers/ProductJpaController.java
@@ -0,0 +1,167 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.controllers;
+
+import model.controllers.exceptions.NonexistentEntityException;
+import model.controllers.exceptions.RollbackFailureException;
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Query;
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.EntityTransaction;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import model.entities.Product;
+
+/**
+ *
+ * @author judith
+ */
+public class ProductJpaController implements Serializable {
+
+    public ProductJpaController(EntityManagerFactory emf) {
+        this.emf = emf;
+    }
+    
+    private EntityManagerFactory emf = null;
+
+    public EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+
+    public void create(Product product) throws RollbackFailureException, Exception {
+        EntityManager em = null;
+        EntityTransaction etx = null;
+        try {
+            em = getEntityManager();
+            etx = em.getTransaction();
+            etx.begin();
+            em.persist(product);
+            etx.commit();
+        } catch (Exception ex) {
+            try {
+                ex.printStackTrace();
+                etx.rollback();
+            } catch (Exception re) {
+                throw new RollbackFailureException("An error occurred attempting to roll back the transaction.", re);
+            }
+            throw ex;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public void edit(Product product) throws NonexistentEntityException, RollbackFailureException, Exception {
+        EntityManager em = null;
+        EntityTransaction etx = null;
+        try {
+            em = getEntityManager();
+            etx = em.getTransaction();
+            etx.begin();
+            product = em.merge(product);
+            etx.commit();
+        } catch (Exception ex) {
+            try {
+                etx.rollback();
+            } catch (Exception re) {
+                throw new RollbackFailureException("An error occurred attempting to roll back the transaction.", re);
+            }
+            String msg = ex.getLocalizedMessage();
+            if (msg == null || msg.length() == 0) {
+                Long id = product.getCodBarras();
+                if (findProduct(id) == null) {
+                    throw new NonexistentEntityException("The product with barcode " + id + " no longer exists.");
+                }
+            }
+            throw ex;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public void destroy(Long id) throws NonexistentEntityException, RollbackFailureException, Exception {
+        EntityManager em = null;
+        EntityTransaction etx = null;
+        try {
+            em = getEntityManager();
+            etx = em.getTransaction();
+            etx.begin();
+            Product product;
+            try {
+                product = em.getReference(Product.class, id);
+                product.getCodBarras();
+            } catch (EntityNotFoundException enfe) {
+                throw new NonexistentEntityException("The product with barcode " + id + " no longer exists.", enfe);
+            }
+            em.remove(product);
+            etx.commit();
+        } catch (Exception ex) {
+            try {
+                etx.rollback();
+            } catch (Exception re) {
+                throw new RollbackFailureException("An error occurred attempting to roll back the transaction.", re);
+            }
+            throw ex;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public List<Product> findProductEntities() {
+        return findProductEntities(true, -1, -1);
+    }
+
+    public List<Product> findProductEntities(int maxResults, int firstResult) {
+        return findProductEntities(false, maxResults, firstResult);
+    }
+
+    private List<Product> findProductEntities(boolean all, int maxResults, int firstResult) {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            cq.select(cq.from(Product.class));
+            Query q = em.createQuery(cq);
+            if (!all) {
+                q.setMaxResults(maxResults);
+                q.setFirstResult(firstResult);
+            }
+            return q.getResultList();
+        } finally {
+            em.close();
+        }
+    }
+
+    public Product findProduct(Long id) {
+        EntityManager em = getEntityManager();
+        try {
+            return em.find(Product.class, id);
+        } finally {
+            em.close();
+        }
+    }
+
+    public int getProductCount() {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            Root<Product> rt = cq.from(Product.class);
+            cq.select(em.getCriteriaBuilder().count(rt));
+            Query q = em.createQuery(cq);
+            return ((Long) q.getSingleResult()).intValue();
+        } finally {
+            em.close();
+        }
+    }
+    
+}
