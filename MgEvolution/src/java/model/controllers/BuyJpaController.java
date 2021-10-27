1c40195ea94158a3f2bd0bc4be29b2cb5fb5ae32
Judith Romero <63924732+judithromero23@users.noreply.github.com>
2021-10-27 20:26:00 +0200

    JPA Service and Buy

---
+++ MgEvolution/src/java/model/controllers/BuyJpaController.java
@@ -0,0 +1,166 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.controllers;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Query;
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.EntityTransaction;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import model.controllers.exceptions.NonexistentEntityException;
+import model.controllers.exceptions.RollbackFailureException;
+import model.entities.Buy;
+
+/**
+ *
+ * @author judith
+ */
+public class BuyJpaController implements Serializable {
+
+    public BuyJpaController(EntityManagerFactory emf) {
+        this.emf = emf;
+    }
+    private EntityManagerFactory emf = null;
+
+    public EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+
+    public void create(Buy buy) throws RollbackFailureException, Exception {
+        EntityManager em = null;
+        EntityTransaction etx = null;
+        try {
+            em = getEntityManager();
+            etx = em.getTransaction();
+            etx.begin();
+            em.persist(buy);
+            etx.commit();
+        } catch (Exception ex) {
+            try {
+                etx.rollback();
+            } catch (Exception re) {
+                throw new RollbackFailureException("An error occurred attempting to roll back the transaction.", re);
+            }
+            throw ex;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public void edit(Buy buy) throws NonexistentEntityException, RollbackFailureException, Exception {
+        EntityManager em = null;
+        EntityTransaction etx = null;
+        try {
+            em = getEntityManager();
+            etx = em.getTransaction();
+            etx.begin();
+            buy = em.merge(buy);
+            etx.commit();
+        } catch (Exception ex) {
+            try {
+                etx.rollback();
+            } catch (Exception re) {
+                throw new RollbackFailureException("An error occurred attempting to roll back the transaction.", re);
+            }
+            String msg = ex.getLocalizedMessage();
+            if (msg == null || msg.length() == 0) {
+                Date id = buy.getDate();
+                if (findBuy(id) == null) {
+                    throw new NonexistentEntityException("The buy with id " + id + " no longer exists.");
+                }
+            }
+            throw ex;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public void destroy(Date id) throws NonexistentEntityException, RollbackFailureException, Exception {
+        EntityManager em = null;
+        EntityTransaction etx = null;
+        try {
+            em = getEntityManager();
+            etx = em.getTransaction();
+            etx.begin();
+            Buy buy;
+            try {
+                buy = em.getReference(Buy.class, id);
+                buy.getDate();
+            } catch (EntityNotFoundException enfe) {
+                throw new NonexistentEntityException("The buy with id " + id + " no longer exists.", enfe);
+            }
+            em.remove(buy);
+            etx.commit();
+        } catch (Exception ex) {
+            try {
+                etx.rollback();
+            } catch (Exception re) {
+                throw new RollbackFailureException("An error occurred attempting to roll back the transaction.", re);
+            }
+            throw ex;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public List<Buy> findBuyEntities() {
+        return findBuyEntities(true, -1, -1);
+    }
+
+    public List<Buy> findBuyEntities(int maxResults, int firstResult) {
+        return findBuyEntities(false, maxResults, firstResult);
+    }
+
+    private List<Buy> findBuyEntities(boolean all, int maxResults, int firstResult) {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            cq.select(cq.from(Buy.class));
+            Query q = em.createQuery(cq);
+            if (!all) {
+                q.setMaxResults(maxResults);
+                q.setFirstResult(firstResult);
+            }
+            return q.getResultList();
+        } finally {
+            em.close();
+        }
+    }
+
+    public Buy findBuy(Date id) {
+        EntityManager em = getEntityManager();
+        try {
+            return em.find(Buy.class, id);
+        } finally {
+            em.close();
+        }
+    }
+
+    public int getBuyCount() {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            Root<Buy> rt = cq.from(Buy.class);
+            cq.select(em.getCriteriaBuilder().count(rt));
+            Query q = em.createQuery(cq);
+            return ((Long) q.getSingleResult()).intValue();
+        } finally {
+            em.close();
+        }
+    }
+
+}
---
+++ MgEvolution/src/java/model/controllers/ServiceJpaController.java
@@ -0,0 +1,166 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.controllers;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Query;
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.EntityTransaction;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import model.controllers.exceptions.NonexistentEntityException;
+import model.controllers.exceptions.RollbackFailureException;
+import model.entities.Service;
+
+/**
+ *
+ * @author judith
+ */
+public class ServiceJpaController implements Serializable {
+
+    public ServiceJpaController(EntityManagerFactory emf) {
+        this.emf = emf;
+    }
+    private EntityManagerFactory emf = null;
+
+    public EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+
+    public void create(Service service) throws RollbackFailureException, Exception {
+        EntityManager em = null;
+        EntityTransaction etx = null;
+        try {
+            em = getEntityManager();
+            etx = em.getTransaction();
+            etx.begin();
+            em.persist(service);
+            etx.commit();
+        } catch (Exception ex) {
+            try {
+                etx.rollback();
+            } catch (Exception re) {
+                throw new RollbackFailureException("An error occurred attempting to roll back the transaction.", re);
+            }
+            throw ex;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public void edit(Service service) throws NonexistentEntityException, RollbackFailureException, Exception {
+        EntityManager em = null;
+        EntityTransaction etx = null;
+        try {
+            em = getEntityManager();
+            etx = em.getTransaction();
+            etx.begin();
+            service = em.merge(service);
+            etx.commit();
+        } catch (Exception ex) {
+            try {
+                etx.rollback();
+            } catch (Exception re) {
+                throw new RollbackFailureException("An error occurred attempting to roll back the transaction.", re);
+            }
+            String msg = ex.getLocalizedMessage();
+            if (msg == null || msg.length() == 0) {
+                Date id = service.getDate();
+                if (findService(id) == null) {
+                    throw new NonexistentEntityException("The service with id " + id + " no longer exists.");
+                }
+            }
+            throw ex;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public void destroy(Date id) throws NonexistentEntityException, RollbackFailureException, Exception {
+        EntityManager em = null;
+        EntityTransaction etx = null;
+        try {
+            em = getEntityManager();
+            etx = em.getTransaction();
+            etx.begin();
+            Service service;
+            try {
+                service = em.getReference(Service.class, id);
+                service.getDate();
+            } catch (EntityNotFoundException enfe) {
+                throw new NonexistentEntityException("The service with id " + id + " no longer exists.", enfe);
+            }
+            em.remove(service);
+            etx.commit();
+        } catch (Exception ex) {
+            try {
+                etx.rollback();
+            } catch (Exception re) {
+                throw new RollbackFailureException("An error occurred attempting to roll back the transaction.", re);
+            }
+            throw ex;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public List<Service> findServiceEntities() {
+        return findServiceEntities(true, -1, -1);
+    }
+
+    public List<Service> findServiceEntities(int maxResults, int firstResult) {
+        return findServiceEntities(false, maxResults, firstResult);
+    }
+
+    private List<Service> findServiceEntities(boolean all, int maxResults, int firstResult) {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            cq.select(cq.from(Service.class));
+            Query q = em.createQuery(cq);
+            if (!all) {
+                q.setMaxResults(maxResults);
+                q.setFirstResult(firstResult);
+            }
+            return q.getResultList();
+        } finally {
+            em.close();
+        }
+    }
+
+    public Service findService(Date id) {
+        EntityManager em = getEntityManager();
+        try {
+            return em.find(Service.class, id);
+        } finally {
+            em.close();
+        }
+    }
+
+    public int getServiceCount() {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            Root<Service> rt = cq.from(Service.class);
+            cq.select(em.getCriteriaBuilder().count(rt));
+            Query q = em.createQuery(cq);
+            return ((Long) q.getSingleResult()).intValue();
+        } finally {
+            em.close();
+        }
+    }
+
+}
