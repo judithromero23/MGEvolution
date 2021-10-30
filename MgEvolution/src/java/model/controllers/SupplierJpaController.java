93abe7c870db4fc7c7659577f9e715cb5537722e
Judith Romero <63924732+judithromero23@users.noreply.github.com>
2021-10-30 13:25:52 +0200

    Supplier relation with Product

---
+++ MgEvolution/src/java/model/controllers/SupplierJpaController.java
@@ -0,0 +1,165 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.controllers;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Query;
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.EntityTransaction;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import model.controllers.exceptions.NonexistentEntityException;
+import model.controllers.exceptions.RollbackFailureException;
+import model.entities.Supplier;
+
+/**
+ *
+ * @author judith
+ */
+public class SupplierJpaController implements Serializable {
+
+    public SupplierJpaController(EntityManagerFactory emf) {
+        this.emf = emf;
+    }
+    private EntityManagerFactory emf = null;
+
+    public EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+
+    public void create(Supplier supplier) throws RollbackFailureException, Exception {
+        EntityManager em = null;
+        EntityTransaction etx = null;
+        try {
+            em = getEntityManager();
+            etx = em.getTransaction();
+            etx.begin();
+            em.persist(supplier);
+            etx.commit();
+        } catch (Exception ex) {
+            try {
+                etx.rollback();
+            } catch (Exception re) {
+                throw new RollbackFailureException("An error occurred attempting to roll back the transaction.", re);
+            }
+            throw ex;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public void edit(Supplier supplier) throws NonexistentEntityException, RollbackFailureException, Exception {
+        EntityManager em = null;
+        EntityTransaction etx = null;
+        try {
+            em = getEntityManager();
+            etx = em.getTransaction();
+            etx.begin();
+            supplier = em.merge(supplier);
+            etx.commit();
+        } catch (Exception ex) {
+            try {
+                etx.rollback();
+            } catch (Exception re) {
+                throw new RollbackFailureException("An error occurred attempting to roll back the transaction.", re);
+            }
+            String msg = ex.getLocalizedMessage();
+            if (msg == null || msg.length() == 0) {
+                String id = supplier.getBrand();
+                if (findSupplier(id) == null) {
+                    throw new NonexistentEntityException("The supplier with id " + id + " no longer exists.");
+                }
+            }
+            throw ex;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public void destroy(String id) throws NonexistentEntityException, RollbackFailureException, Exception {
+        EntityManager em = null;
+        EntityTransaction etx = null;
+        try {
+            em = getEntityManager();
+            etx = em.getTransaction();
+            etx.begin();
+            Supplier supplier;
+            try {
+                supplier = em.getReference(Supplier.class, id);
+                supplier.getBrand();
+            } catch (EntityNotFoundException enfe) {
+                throw new NonexistentEntityException("The supplier with id " + id + " no longer exists.", enfe);
+            }
+            em.remove(supplier);
+            etx.commit();
+        } catch (Exception ex) {
+            try {
+                etx.rollback();
+            } catch (Exception re) {
+                throw new RollbackFailureException("An error occurred attempting to roll back the transaction.", re);
+            }
+            throw ex;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public List<Supplier> findSupplierEntities() {
+        return findSupplierEntities(true, -1, -1);
+    }
+
+    public List<Supplier> findSupplierEntities(int maxResults, int firstResult) {
+        return findSupplierEntities(false, maxResults, firstResult);
+    }
+
+    private List<Supplier> findSupplierEntities(boolean all, int maxResults, int firstResult) {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            cq.select(cq.from(Supplier.class));
+            Query q = em.createQuery(cq);
+            if (!all) {
+                q.setMaxResults(maxResults);
+                q.setFirstResult(firstResult);
+            }
+            return q.getResultList();
+        } finally {
+            em.close();
+        }
+    }
+
+    public Supplier findSupplier(String id) {
+        EntityManager em = getEntityManager();
+        try {
+            return em.find(Supplier.class, id);
+        } finally {
+            em.close();
+        }
+    }
+
+    public int getSupplierCount() {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            Root<Supplier> rt = cq.from(Supplier.class);
+            cq.select(em.getCriteriaBuilder().count(rt));
+            Query q = em.createQuery(cq);
+            return ((Long) q.getSingleResult()).intValue();
+        } finally {
+            em.close();
+        }
+    }
+    
+}
