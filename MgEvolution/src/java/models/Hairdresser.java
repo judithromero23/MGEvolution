6bf71ed1d88091e310ef23b8e433d3d5b44d3929
Judith Romero <63924732+judithromero23@users.noreply.github.com>
2021-11-14 20:02:12 +0100

    All entities

--- MgEvolution/src/java/models/Hairdresser.java
+++ MgEvolution/src/java/models/Hairdresser.java
@@ -10,10 +10,16 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import javax.persistence.Persistence;
+import model.controllers.BuyJpaController;
+import model.controllers.ClientJpaController;
 import model.controllers.ProductJpaController;
+import model.controllers.ServiceJpaController;
 import model.controllers.StylistJpaController;
 import model.controllers.SupplierJpaController;
+import model.entities.Buy;
+import model.entities.Client;
 import model.entities.Product;
+import model.entities.Service;
 import model.entities.Stylist;
 import model.entities.Supplier;
 
@@ -22,11 +28,11 @@ import model.entities.Supplier;
  * @author judith
  */
 public class Hairdresser {
-    
+
     //Persistence
     public static final String PERSISTENCIA = "MgEvolutionPU";
 
-    //Stylist
+//Stylist
     public List<Stylist> getStylist() {
         StylistJpaController ejc = new StylistJpaController(Persistence.createEntityManagerFactory(PERSISTENCIA));
         return ejc.findStylistEntities();
@@ -73,58 +79,7 @@ public class Hairdresser {
         StylistJpaController ejc = new StylistJpaController(Persistence.createEntityManagerFactory(PERSISTENCIA));
         ejc.edit(stylist);
     }
-    
-    
-//Product
-    public List<Product> getProduct() {
-        ProductJpaController ejc = new ProductJpaController(Persistence.createEntityManagerFactory(PERSISTENCIA));
-        return ejc.findProductEntities();
-    }
-
-    public List<Product> getProducttAlfabeticamente() {
-        List<Product> product = getProduct();
-        Collections.sort(product, (e1, e2)
-                -> (e1.getName()).compareTo(e2.getName()));
-        return product;
-    }
-
-    public List<Product> filterProduct(String filtro) {
-        List<Product> product = getProduct();
-        List<Product> filter = new ArrayList();
-        if (!filtro.isEmpty()) {
-            for (Product e : product) {
-                if (Objects.equals(e.getCodBarras(), filtro)) {
-                    filter.add(e);
-                }
-            }
-        } else {
-            filter = product;
-        }
-        return filter;
-    }
-
-    public void deleteProduct(Long id) throws Exception {
-        ProductJpaController ejc = new ProductJpaController(Persistence.createEntityManagerFactory(PERSISTENCIA));
-        ejc.destroy(id);
-    }
-
-    public void addProduct(Product product) throws Exception {
-        ProductJpaController ejc = new ProductJpaController(Persistence.createEntityManagerFactory(PERSISTENCIA));
-        ejc.create(product);
-    }
-
-    public Product searchProduct(Long codBarras) {
-        ProductJpaController ejc = new ProductJpaController(Persistence.createEntityManagerFactory(PERSISTENCIA));
-        return ejc.findProduct(codBarras);
-    }
-
-    public void updateProduct(Product product) throws Exception {
-        ProductJpaController ejc = new ProductJpaController(Persistence.createEntityManagerFactory(PERSISTENCIA));
-        ejc.edit(product);
-    }
-    
-    
-    //Supplier
+//Supplier
     public List<Supplier> getSupplier() {
         SupplierJpaController ejc = new SupplierJpaController(Persistence.createEntityManagerFactory(PERSISTENCIA));
         return ejc.findSupplierEntities();
@@ -171,6 +126,184 @@ public class Hairdresser {
         SupplierJpaController ejc = new SupplierJpaController(Persistence.createEntityManagerFactory(PERSISTENCIA));
         ejc.edit(supplier);
     }
+    
+    
+//Product 
+    public List<Product> getProduct() {
+        ProductJpaController ejc = new ProductJpaController(Persistence.createEntityManagerFactory(PERSISTENCIA));
+        return ejc.findProductEntities();
+    }
+
+    public List<Product> getProductAlfabeticamente() {
+        List<Product> product = getProduct();
+        Collections.sort(product, (e1, e2)
+                -> (e1.getName()).compareTo(e2.getName()));
+        return product;
+    }
+
+    public List<Product> filterProduct(String filtro) {
+        List<Product> product = getProduct();
+        List<Product> filter = new ArrayList();
+        if (!filtro.isEmpty()) {
+            for (Product e : product) {
+                if (Objects.equals(e.getCodBarras(), filtro)) {
+                    filter.add(e);
+                }
+            }
+        } else {
+            filter = product;
+        }
+        return filter;
+    }
+
+    public void deleteProduct(Long codBarras) throws Exception {
+        ProductJpaController ejc = new ProductJpaController(Persistence.createEntityManagerFactory(PERSISTENCIA));
+        ejc.destroy(codBarras);
+    }
+
+    public void addProduct(Product product) throws Exception {
+        ProductJpaController ejc = new ProductJpaController(Persistence.createEntityManagerFactory(PERSISTENCIA));
+        ejc.create(product);
+    }
+
+    public Product searchProduct(Long codBarras) {
+        ProductJpaController ejc = new ProductJpaController(Persistence.createEntityManagerFactory(PERSISTENCIA));
+        return ejc.findProduct(codBarras);
+    }
+
+    public void updateProduct(Product product) throws Exception {
+        ProductJpaController ejc = new ProductJpaController(Persistence.createEntityManagerFactory(PERSISTENCIA));
+        ejc.edit(product);
+    }
+
+   
+//Client 
+    public List<Client> getClient() {
+        ClientJpaController ejc = new ClientJpaController(Persistence.createEntityManagerFactory(PERSISTENCIA));
+        return ejc.findClientEntities();
+    }
+
+    public List<Client> getClientAlfabeticamente() {
+        List<Client> client = getClient();
+        Collections.sort(client, (e1, e2)
+                -> (e1.getName()).compareTo(e2.getName()));
+        return client;
+    }
+
+    public List<Client> filterClient(String filtro) {
+        List<Client> client = getClient();
+        List<Client> filter = new ArrayList();
+        if (!filtro.isEmpty()) {
+            for (Client e : client) {
+                if (Objects.equals(e.getDNI(), filtro)) {
+                    filter.add(e);
+                }
+            }
+        } else {
+            filter = client;
+        }
+        return filter;
+    }
+
+    public void deleteClient(Long DNI) throws Exception {
+        ClientJpaController ejc = new ClientJpaController(Persistence.createEntityManagerFactory(PERSISTENCIA));
+        ejc.destroy(DNI);
+    }
+
+    public void addClient(Client client) throws Exception {
+        ClientJpaController ejc = new ClientJpaController(Persistence.createEntityManagerFactory(PERSISTENCIA));
+        ejc.create(client);
+    }
+
+    public Client searchClient(Long DNI) {
+        ClientJpaController ejc = new ClientJpaController(Persistence.createEntityManagerFactory(PERSISTENCIA));
+        return ejc.findClient(DNI);
+    }
+
+    public void updateClient(Client client) throws Exception {
+        ClientJpaController ejc = new ClientJpaController(Persistence.createEntityManagerFactory(PERSISTENCIA));
+        ejc.edit(client);
+    }
+    
+//Service 
+    public List<Service> getService() {
+        ServiceJpaController ejc = new ServiceJpaController(Persistence.createEntityManagerFactory(PERSISTENCIA));
+        return ejc.findServiceEntities();
+    }
+
+    public List<Service> filterService(String filtro) {
+        List<Service> service = getService();
+        List<Service> filter = new ArrayList();
+        if (!filtro.isEmpty()) {
+            for (Service e : service) {
+                if (Objects.equals(e.getId(), filtro)) {
+                    filter.add(e);
+                }
+            }
+        } else {
+            filter = service;
+        }
+        return filter;
+    }
+
+    public void deleteService(Long id) throws Exception {
+        ServiceJpaController ejc = new ServiceJpaController(Persistence.createEntityManagerFactory(PERSISTENCIA));
+        ejc.destroy(id);
+    }
+
+    public void addService(Service service) throws Exception {
+        ServiceJpaController ejc = new ServiceJpaController(Persistence.createEntityManagerFactory(PERSISTENCIA));
+        ejc.create(service);
+    }
+
+    public Service searchService(Long id) {
+        ServiceJpaController ejc = new ServiceJpaController(Persistence.createEntityManagerFactory(PERSISTENCIA));
+        return ejc.findService(id);
+    }
+
+    public void updateService(Service service) throws Exception {
+        ServiceJpaController ejc = new ServiceJpaController(Persistence.createEntityManagerFactory(PERSISTENCIA));
+        ejc.edit(service);
+    }
+    
+//Buy 
+    public List<Buy> getBuy() {
+        BuyJpaController ejc = new BuyJpaController(Persistence.createEntityManagerFactory(PERSISTENCIA));
+        return ejc.findBuyEntities();
+    }
+
+    public List<Buy> filterBuy(String filtro) {
+        List<Buy> buy = getBuy();
+        List<Buy> filter = new ArrayList();
+        if (!filtro.isEmpty()) {
+            for (Buy e : buy) {
+                if (Objects.equals(e.getNumTicket(), filtro)) {
+                    filter.add(e);
+                }
+            }
+        } else {
+            filter = buy;
+        }
+        return filter;
+    }
+
+    public void deleteBuy(Long numTicket) throws Exception {
+        BuyJpaController ejc = new BuyJpaController(Persistence.createEntityManagerFactory(PERSISTENCIA));
+        ejc.destroy(numTicket);
+    }
+
+    public void addBuy(Buy buy) throws Exception {
+        BuyJpaController ejc = new BuyJpaController(Persistence.createEntityManagerFactory(PERSISTENCIA));
+        ejc.create(buy);
+    }
+
+    public Buy searchBuy(Long numTicket) {
+        BuyJpaController ejc = new BuyJpaController(Persistence.createEntityManagerFactory(PERSISTENCIA));
+        return ejc.findBuy(numTicket);
+    }
+
+    public void updateBuy(Buy buy) throws Exception {
+        BuyJpaController ejc = new BuyJpaController(Persistence.createEntityManagerFactory(PERSISTENCIA));
+        ejc.edit(buy);
+    }
 }
-
-
